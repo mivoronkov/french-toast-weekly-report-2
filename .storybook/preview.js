068c9dd46458b708c2fe2072a8f2b5919048a73d
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-02 19:47:53 +0300

    fix: deleted not working img imports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,10 +10,3 @@ export const parameters = {
 
 import '../src/styles/main.scss';
 import 'bootstrap';
-import "../src/styles/main.scss";
-import "../src/img/happy.svg";
-import "../src/img/very-happy.svg";
-import "../src/img/very-sad.svg";
-import "../src/img/neutral.svg";
-import "../src/img/sad.svg";
-import "../src/img/unmarked.svg";
