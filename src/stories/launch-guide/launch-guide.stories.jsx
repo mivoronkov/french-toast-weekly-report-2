cb0950e497253ccc590825718a7fc6b06a175964
koshelevandrey <tikr22@gmail.com>
2021-12-03 08:14:19 +0300

    feat[launch guide]: added stories

---
+++ src/stories/launch-guide/launch-guide.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import LaunchGuideComponent from "../../modules/launch-guide/launch-guide.component";
+
+export default {
+    title: 'Launch Guide',
+    component: LaunchGuideComponent
+}
+
+const Template = () => <LaunchGuideComponent />;
+export const LaunchGuideTemplate = Template.bind({});
