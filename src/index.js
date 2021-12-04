56ebd0f4eaf337dc99ce77a5297fd8a127e4d39c
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-04 10:07:39 +0300

    feat: pre-commit style check

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { App } from './modules/app/app.component'
-import reportWebVitals from './reportWebVitals'
-import './styles/main.scss'
-import 'bootstrap'
-import { FeedbackButtonComponent } from './modules/common/components/buttons/feedback-button.component'
-import { HelpButtonComponent } from './modules/common/components/buttons/help-button.component'
+import { App } from './modules/app/app.component';
+import reportWebVitals from './reportWebVitals';
+import './styles/main.scss';
+import 'bootstrap';
+import { FeedbackButtonComponent } from './modules/common/components/buttons/feedback-button.component';
+import { HelpButtonComponent } from './modules/common/components/buttons/help-button.component';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -15,9 +15,9 @@ ReactDOM.render(
         <App />
     </React.StrictMode>,
     document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
