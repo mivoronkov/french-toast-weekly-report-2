065b25dbcf463bc90cfa549aaf58946e80ae7d8e
Alexander Slesarenko <dragongling@gmail.com>
2021-12-06 15:58:47 +0500

    feat: edit field component

---
+++ src/modules/edit-member-information/edit-field.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getRandomId, toDashCase } from '../../utils';
+
+export function EditFieldComponent({ label, width = '100%', value = '' }) {
+    const id = label ? toDashCase(label) : getRandomId();
+    const label_tag = label ? (
+        <label htmlFor={id} className='fw-bold'>
+            {label}
+        </label>
+    ) : null;
+    return (
+        <div className='form-group mt-2 mb-2'>
+            {label_tag}
+            <input
+                type='text'
+                className='form-control'
+                id={id}
+                value={value}
+                width={width}
+            />
+        </div>
+    );
+}
+
+EditFieldComponent.propTypes = {
+    label: PropTypes.string,
+    value: PropTypes.string,
+    width: PropTypes.string,
+};
