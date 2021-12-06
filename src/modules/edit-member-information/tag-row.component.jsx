b40a7df1ae81c6b03ec8d98810f7df821539d546
Alexander Slesarenko <dragongling@gmail.com>
2021-12-06 15:26:12 +0500

    feat: tag row component

---
+++ src/modules/edit-member-information/tag-row.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function TagRowComponent({ tag_names }) {
+    let tags = tag_names.map((name, index) => (
+        <div className='btn btn-dark' key={index}>
+            {name}
+        </div>
+    ));
+    return <div className='d-flex flex-row tag-row flex-wrap'>{tags}</div>;
+}
+
+TagRowComponent.propTypes = {
+    tag_names: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
