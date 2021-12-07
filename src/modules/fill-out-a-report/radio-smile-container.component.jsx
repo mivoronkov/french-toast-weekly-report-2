9233e04c3d072ccf101b6f11921f37565275cf3a
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-07 09:21:10 +0300

    refract [modules/fill-out-report/radio-smile]: used prettier

--- src/modules/fill-out-a-report/radio-smile-container.component.jsx
+++ src/modules/fill-out-a-report/radio-smile-container.component.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {RadioSmile} from './smile-radio.component';
+import { RadioSmile } from './smile-radio.component';
 
-export function RadioSmileContainer({mainLabel, containerInputName, containerLabels}){
+export function RadioSmileContainer({
+    mainLabel,
+    containerInputName,
+    containerLabels,
+}) {
     const stubArray = Array(5).fill(0);
-    let radioButtons =  stubArray.map((_, index) =>{
+    let radioButtons = stubArray.map((_, index) => {
         return (
             <RadioSmile
-                valueLevel={index+1}
+                valueLevel={index + 1}
                 inputName={containerInputName}
                 valueText={containerLabels[index]}
-                key ={index}
+                key={index}
             />
         );
     });
 
-    return(
-        <div className="mt-5">
-            <h2 className="content-block-title fw-bold p-4">How was your morale this week?</h2>
-            <div className="row">
-                {radioButtons}
-            </div>
+    return (
+        <div className='mt-5'>
+            <h2 className='content-block-title fw-bold p-4'>{mainLabel}</h2>
+            <div className='row'>{radioButtons}</div>
         </div>
     );
-};
+}
 
-RadioSmileContainer.propTypes ={
+RadioSmileContainer.propTypes = {
     mainLabel: PropTypes.string,
     containerInputName: PropTypes.string,
-    containerLabels: PropTypes.arrayOf(PropTypes.string)
+    containerLabels: PropTypes.arrayOf(PropTypes.string),
 };
