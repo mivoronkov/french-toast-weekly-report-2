2598ac8572e946c7827b67e181ec5f615d9f969a
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-08 14:49:11 +0300

    feat: my reports page

--- src/modules/pages/my-reports/my-reports.component.jsx
+++ src/modules/pages/my-reports/my-reports.component.jsx
@@ -3,15 +3,41 @@ import PropTypes from 'prop-types';
 import { ProfileHeaderComponent } from '../../headers/profile-header/profile-header.component';
 import { MyReportsContent } from '../../my-reports/my-reports-contetnt.component';
 
-export function MyReports() {
+export function MyReports({ first_name, last_name, email, avatar_path, data }) {
     return (
         <main className='flex-grow-1 overflow-auto'>
-            <ProfileHeaderComponent members={members} />
-            <MyReportsContent
-                previousPeriod={previousPeriod}
-                currentPeriod={currentPeriod}
-                members={members}
+            <ProfileHeaderComponent
+                first_name={first_name}
+                email={email}
+                last_name={last_name}
+                avatar_path={avatar_path}
             />
+            <MyReportsContent data={data} />
         </main>
     );
 }
+
+MyReports.propTypes = {
+    first_name: PropTypes.string.isRequired,
+    last_name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    avatar_path: PropTypes.string,
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+            duration: PropTypes.string,
+            weeklyInformation: PropTypes.arrayOf(
+                PropTypes.shape({
+                    stateName: PropTypes.string,
+                    stateLevel: PropTypes.number,
+                    comments: PropTypes.string,
+                })
+            ),
+            weeklyNotations: PropTypes.arrayOf(
+                PropTypes.shape({
+                    text: PropTypes.string,
+                    title: PropTypes.string,
+                })
+            ),
+        })
+    ),
+};
