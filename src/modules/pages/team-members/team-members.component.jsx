b1eba841b01bcd78781ae3d573b532433866e3f9
koshelevandrey <tikr22@gmail.com>
2021-12-08 10:06:58 +0300

    fix: refactored team members page

--- src/modules/pages/team-members/team-members.component.jsx
+++ src/modules/pages/team-members/team-members.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TextWithDateHeaderComponent } from '../headers/text-with-date-header/text-with-date-header.component';
-import { TeamMembersContent } from './team-members-content.component';
+import { TextWithDateHeaderComponent } from '../../headers/text-with-date-header/text-with-date-header.component';
+import { TeamMembersContent } from '../../team-members/team-members-content.component';
 
 export function TeamMembers({ companyName, joinedDate, members }) {
     return (
