149986d500683b0f79643a016870c0785e06f404
koshelevandrey <tikr22@gmail.com>
2021-12-08 09:53:38 +0300

    fix: fixed app component

--- src/modules/app/app.component.jsx
+++ src/modules/app/app.component.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
 import { SidebarComponent } from '../common/components/sidebar/sidebar.component';
-import { LogoHeaderComponent } from '../headers/logo-header/logo-header.component';
-import { LaunchGuideContent } from '../launch-guide/launch-guide-content.component';
+import { LaunchGuide } from '../pages/launch-guide/launch-guide.component';
 
 export function App() {
     return (
         <div className='d-flex h-100'>
             <SidebarComponent />
-            <main className='flex-grow-1 overflow-auto'>
-                <LogoHeaderComponent title={'Launch Guide'} />
-                <LaunchGuideContent />
-            </main>
+            <LaunchGuide />
         </div>
     );
 }
