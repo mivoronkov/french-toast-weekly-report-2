129c6b63300a1bd65b5dfd0ce55cf50992bbbd1b
Alexander Slesarenko <dragongling@gmail.com>
2021-12-09 09:28:17 +0500

    refactoring: team members in edit member information stories

--- src/modules/pages/edit-member-information/edit-member-information.stories.jsx
+++ src/modules/pages/edit-member-information/edit-member-information.stories.jsx
@@ -6,6 +6,79 @@ export default {
     component: EditMemberInformation,
 };
 
+const testMembers = [
+    {
+        id: 0,
+        firstName: 'Nikolai',
+        lastName: 'Kapustin',
+    },
+    {
+        id: 1,
+        firstName: 'Nina',
+        lastName: 'Mammadova',
+    },
+    {
+        id: 2,
+        firstName: 'Natalia',
+        lastName: 'Starkova',
+    },
+    {
+        id: 3,
+        firstName: 'Anton',
+        lastName: 'Tarkhanov',
+    },
+    {
+        id: 4,
+        firstName: 'Alexandr',
+        lastName: 'Vovchuk',
+    },
+    {
+        id: 5,
+        firstName: 'Aleksandr',
+        lastName: 'Evseev',
+    },
+    {
+        id: 6,
+        firstName: 'Anna',
+        lastName: 'Kotova',
+    },
+    {
+        id: 7,
+        firstName: 'Anatoliy',
+        lastName: 'Kolodkin',
+    },
+    {
+        id: 8,
+        firstName: 'Alexander',
+        lastName: 'Slesarenko',
+    },
+    {
+        id: 9,
+        firstName: 'Andrey',
+        lastName: 'Koshelev',
+    },
+    {
+        id: 10,
+        firstName: 'Anton',
+        lastName: 'Semenkov',
+    },
+    {
+        id: 11,
+        firstName: 'Ilya',
+        lastName: 'Krasnoperov',
+    },
+    {
+        id: 12,
+        firstName: 'Anton',
+        lastName: 'Makarov',
+    },
+    {
+        id: 13,
+        firstName: 'Anton',
+        lastName: 'Kazakevich',
+    },
+];
+
 const Template = (args) => <EditMemberInformation {...args} />;
 export const Variant1 = Template.bind({});
 Variant1.args = {
@@ -16,69 +89,22 @@ Variant1.args = {
     inviteLink:
         'https://weeklyreport.entreleadership.com/accept/eyJsaWQiOiDFjaeFHjewlskdJFosdgSfjsdkHFJLGewjmds',
     leadersToReport: [
-        {
-            id: 1,
-            firstName: 'Nikolai',
-            lastName: 'Kapustin',
-        },
-        {
-            id: 2,
-            firstName: 'Nina',
-            lastName: 'Mammadova',
-        },
-        {
-            id: 3,
-            firstName: 'Natalia',
-            lastName: 'Starkova',
-        },
-        {
-            id: 4,
-            firstName: 'Anton',
-            lastName: 'Tarkhanov',
-        },
-        {
-            id: 5,
-            firstName: 'Alexandr',
-            lastName: 'Vovchuk',
-        },
+        testMembers[0],
+        testMembers[1],
+        testMembers[2],
+        testMembers[3],
+        testMembers[4],
     ],
     reportingMembers: [
-        {
-            id: 6,
-            firstName: 'Aleksandr',
-            lastName: 'Evseev',
-        },
-        {
-            id: 1,
-            firstName: 'Nikolai',
-            lastName: 'Kapustin',
-        },
-        {
-            id: 8,
-            firstName: 'Anna',
-            lastName: 'Kotova',
-        },
-        {
-            id: 2,
-            firstName: 'Nina',
-            lastName: 'Mammadova',
-        },
-        {
-            id: 3,
-            firstName: 'Natalia',
-            lastName: 'Starkova',
-        },
-        {
-            id: 4,
-            firstName: 'Anton',
-            lastName: 'Tarkhanov',
-        },
-        {
-            id: 5,
-            firstName: 'Alexandr',
-            lastName: 'Vovchuk',
-        },
+        testMembers[6],
+        testMembers[0],
+        testMembers[7],
+        testMembers[1],
+        testMembers[2],
+        testMembers[3],
+        testMembers[4],
     ],
+    allMembers: testMembers,
 };
 
 export const Variant2 = Template.bind({});
