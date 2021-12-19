1bf026dcbccbbfa4d38eea6b43da6514057e3466
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-19 14:02:12 +0300

    refract [axios]: move token to localstorage

--- src/modules/api/auth-jwt-store.js
+++
@@ -1,8 +0,0 @@
-import { createEvent, createStore } from 'effector';
-
-export const setToken = createEvent();
-export const resetToken = createEvent();
-
-export const tokenStore = createStore('')
-    .on(setToken, (_, payload) => payload)
-    .reset(resetToken);
