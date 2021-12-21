34d78210d95af9c35f69f364e4d75f5f35b3a2f0
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-21 15:50:09 +0300

    feat [stores]: changed stores

---
+++ src/modules/store/all-companies-store.js
@@ -0,0 +1,16 @@
+import { createStore, createEffect } from 'effector';
+import { apiInvoker } from '../api/api-axios';
+
+export const getCompanies = createEffect(async () => {
+    const resp = await apiInvoker.companies.getAll();
+    return resp.data;
+});
+export const createCompany = createEffect(async (companyName) => {
+    const resp = await apiInvoker.companies.create(companyName);
+    await getCompanies();
+    return resp.data;
+});
+export const allCompaniesStore = createStore([]);
+allCompaniesStore.on(getCompanies.doneData, (_, data) => {
+    return data;
+});
