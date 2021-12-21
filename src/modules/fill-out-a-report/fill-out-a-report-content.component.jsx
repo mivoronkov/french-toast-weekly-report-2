4ae3eefb8d4d20059ee5265352d7e305350cdab8
koshelevandrey <tikr22@gmail.com>
2021-12-21 16:34:51 +0300

    feat: added formik to fill out a report page

--- src/modules/fill-out-a-report/fill-out-a-report-content.component.jsx
+++ src/modules/fill-out-a-report/fill-out-a-report-content.component.jsx
@@ -5,8 +5,13 @@ import { TextAreaCounter } from './text-area-counter.component';
 import moment from 'moment';
 import DateRangePicker from 'react-bootstrap-daterangepicker';
 import 'bootstrap-daterangepicker/daterangepicker.css';
+import { Form, Formik, Field, useFormikContext } from 'formik';
+import { useStore } from 'effector-react';
+import { userStore } from '../store/user-store';
 
 export function FillOutAReportContent({ firstName }) {
+    const userInDB = useStore(userStore);
+
     const [dateState, setDateState] = useState({
         startDate: moment().subtract(7, 'days'),
         endDate: moment(),
@@ -36,6 +41,24 @@ export function FillOutAReportContent({ firstName }) {
 
     let containerLabels = ['Very Low', 'Low', 'Okay', 'Good', 'Great'];
     let maxTextAreaLength = 600;
+
+    const initFormValues = {
+        moraleInput: '0',
+        moraleCommentTextarea: '',
+        stressInput: '0',
+        stressCommentTextarea: '',
+        workloadInput: '0',
+        workloadCommentTextarea: '',
+        highTextarea: '',
+        lowTextarea: '',
+        elseTextarea: '',
+        date: '',
+    };
+
+    const onSubmit = async (values) => {
+        /* TODO: make API call to send weekly report */
+    };
+
     return (
         <div className='d-flex flex-column align-items-center w-100 text-center'>
             <h1 className='big-title brush-bg fw-bold w-100'>
@@ -47,95 +70,119 @@ export function FillOutAReportContent({ firstName }) {
                 Let your leader know where you&apos;re winning and struggling
                 this week – in less than 10 minutes.
             </div>
-            <form>
-                <div className='mx-auto'>
-                    <RadioSmileContainer
-                        mainLabel={'How was your morale this week?'}
-                        containerInputName={'moraleInput'}
-                        containerLabels={containerLabels}
-                        commentTextareaId={'moraleCommentTextarea'}
-                    />
-                    <RadioSmileContainer
-                        mainLabel={'How was your stress this week?'}
-                        containerInputName={'stressInput'}
-                        containerLabels={containerLabels}
-                        commentTextareaId={'stressCommentTextarea'}
-                    />
-                    <RadioSmileContainer
-                        mainLabel={'How was your workload this week?'}
-                        containerInputName={'workloadInput'}
-                        containerLabels={containerLabels}
-                        commentTextareaId={'workloadCommentTextarea'}
-                    />
-                    <TextAreaCounter
-                        labelText={'What was your high this week?'}
-                        placeholderText={
-                            "What was your personal or professional high this week? What's the one thing you accomplished at work this week?"
-                        }
-                        idText={'whats-high-textarea'}
-                        maxLength={maxTextAreaLength}
-                    />
-                    <TextAreaCounter
-                        labelText={'What was your low this week?'}
-                        placeholderText={
-                            'What was your personal low this week?'
-                        }
-                        idText={'whats-low-textarea'}
-                        maxLength={maxTextAreaLength}
-                    />
-                    <TextAreaCounter
-                        labelText={'Anything else?'}
-                        placeholderText={
-                            'Is there anything else you would like to share with your leader? *Optional'
-                        }
-                        idText={'anything-else-textarea'}
-                        maxLength={maxTextAreaLength}
-                    />
-                </div>
-                <div className='mt-5 mb-5 small-block'>
-                    <h2 className='content-block-title fw-bold p-4'>
-                        Date range
-                    </h2>
-                    <div className='mt-3 mb-5'>
-                        <div className='text-start mb-1'>
-                            <label>Choose date</label>
-                        </div>
-                        <div className='input-group d-flex flex-row'>
-                            <input
-                                type='text'
-                                className='form-control date-range-picker-input bg-white'
-                                value={dateLabel}
-                                aria-label="Recipient's username"
-                                aria-describedby='button-addon2'
-                                readOnly={true}
-                            />
-                            <DateRangePicker
-                                initialSettings={{
-                                    startDate: dateState.startDate.toDate(),
-                                    endDate: dateState.endDate.toDate(),
-                                }}
-                                onCallback={handleDateCallback}>
-                                <button
-                                    className='btn btn-outline-secondary'
-                                    type='button'
-                                    id='button-addon2'>
-                                    ↕
-                                </button>
-                            </DateRangePicker>
+            <Formik
+                initialValues={initFormValues}
+                onSubmit={onSubmit}
+                enableReinitialize={true}>
+                <Form>
+                    <div className='mx-auto'>
+                        <RadioSmileContainer
+                            mainLabel={'How was your morale this week?'}
+                            containerInputName={'moraleInput'}
+                            containerLabels={containerLabels}
+                            commentTextareaId={'moraleCommentTextarea'}
+                        />
+                        <RadioSmileContainer
+                            mainLabel={'How was your stress this week?'}
+                            containerInputName={'stressInput'}
+                            containerLabels={containerLabels}
+                            commentTextareaId={'stressCommentTextarea'}
+                        />
+                        <RadioSmileContainer
+                            mainLabel={'How was your workload this week?'}
+                            containerInputName={'workloadInput'}
+                            containerLabels={containerLabels}
+                            commentTextareaId={'workloadCommentTextarea'}
+                        />
+                        <TextAreaCounter
+                            labelText={'What was your high this week?'}
+                            placeholderText={
+                                "What was your personal or professional high this week? What's the one thing you accomplished at work this week?"
+                            }
+                            idText={'whats-high-textarea'}
+                            textareaName={'highTextarea'}
+                            maxLength={maxTextAreaLength}
+                        />
+                        <TextAreaCounter
+                            labelText={'What was your low this week?'}
+                            placeholderText={
+                                'What was your personal low this week?'
+                            }
+                            idText={'whats-low-textarea'}
+                            textareaName={'lowTextarea'}
+                            maxLength={maxTextAreaLength}
+                        />
+                        <TextAreaCounter
+                            labelText={'Anything else?'}
+                            placeholderText={
+                                'Is there anything else you would like to share with your leader? *Optional'
+                            }
+                            idText={'anything-else-textarea'}
+                            textareaName={'elseTextarea'}
+                            maxLength={maxTextAreaLength}
+                        />
+                    </div>
+                    <div className='mt-5 mb-5 small-block'>
+                        <h2 className='content-block-title fw-bold p-4'>
+                            Date range
+                        </h2>
+                        <div className='mt-3 mb-5'>
+                            <div className='text-start mb-1'>
+                                <label>Choose date</label>
+                            </div>
+                            <div className='input-group d-flex flex-row'>
+                                <Field
+                                    type='text'
+                                    name='date'
+                                    className='form-control date-range-picker-input bg-white'
+                                    id='datepickerId'
+                                    value={dateLabel}
+                                    aria-label="Recipient's username"
+                                    aria-describedby='button-addon2'
+                                />
+                                {/*<Field
+                                    type='text'
+                                    name='date'
+                                    className='form-control date-range-picker-input bg-white'
+                                    value={dateLabel}
+                                    readOnly={true}
+                                />*/}
+                                {/*<input
+                                    type='text'
+                                    className='form-control date-range-picker-input bg-white'
+                                    value={dateLabel}
+                                    aria-label="Recipient's username"
+                                    aria-describedby='button-addon2'
+                                    readOnly={true}
+                                />*/}
+                                <DateRangePicker
+                                    initialSettings={{
+                                        startDate: dateState.startDate.toDate(),
+                                        endDate: dateState.endDate.toDate(),
+                                    }}
+                                    onCallback={handleDateCallback}>
+                                    <button
+                                        className='btn btn-outline-secondary'
+                                        type='button'
+                                        id='button-addon2'>
+                                        ↕
+                                    </button>
+                                </DateRangePicker>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='mb-5 small-block'>
-                    <p className='text-danger fw-bold'>
-                        All fields are required unless marked as optional.
-                    </p>
-                    <button
-                        type={'submit'}
-                        className='btn btn-primary disabled w-100 send-report-button'>
-                        Send Weekly Report
-                    </button>
-                </div>
-            </form>
+                    <div className='mb-5 small-block'>
+                        <p className='text-danger fw-bold'>
+                            All fields are required unless marked as optional.
+                        </p>
+                        <button
+                            type='submit'
+                            className='btn btn-primary w-100 send-report-button bg-dark'>
+                            Send Weekly Report
+                        </button>
+                    </div>
+                </Form>
+            </Formik>
         </div>
     );
 }
--- src/modules/fill-out-a-report/text-area-counter.component.jsx
+++ src/modules/fill-out-a-report/text-area-counter.component.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Field, useFormikContext } from 'formik';
 
 export function TextAreaCounter({
     labelText,
     placeholderText,
     idText,
+    textareaName,
     maxLength,
 }) {
     const [charCounter, setCharCounter] = useState(0);
+    const context = useFormikContext();
     const changeHandler = (event) => {
         setCharCounter(event.target.value.length);
+        context.setFieldValue(textareaName, event.target.value);
     };
+
     return (
         <div className='mt-5'>
             <h2 className='content-block-title fw-bold text-center p-4'>
                 <label htmlFor={idText}>{labelText}</label>
             </h2>
             <div className='textarea-with-counter'>
-                <textarea
+                <Field
                     id={idText}
+                    name={textareaName}
+                    component='textarea'
                     className='form-control fill-out-textarea'
                     placeholder={placeholderText}
                     maxLength={maxLength}
@@ -36,5 +43,6 @@ TextAreaCounter.propTypes = {
     labelText: PropTypes.string,
     placeholderText: PropTypes.string,
     idText: PropTypes.string,
+    textareaName: PropTypes.string.isRequired,
     maxLength: PropTypes.number,
 };
