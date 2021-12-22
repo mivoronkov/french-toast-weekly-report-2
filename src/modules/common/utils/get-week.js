a96513d4b5ca79ddf381cd2ffe3c4bf72da0253b
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-22 16:42:18 +0300

    feat [utils]: current and previous weeks

--- src/modules/common/utils/get-week.js
+++ src/modules/common/utils/get-week.js
@@ -1,10 +1,30 @@
 export function getWeek(d) {
-    d = new Date(d);
-    const day = d.getDay();
-    const monday = d.getDate() - day + (day == 0 ? -6:1);
-    const sunday = d.getDate() - day + (day == 0 ? 1:7);
+    const date = new Date(d);
+    const day = date.getDay();
+    const monday = date.getDate() - day + (day == 0 ? -6:1);
+    const sunday = date.getDate() - day + (day == 0 ? 1:7);
     return {
-        monday: new Date(d.setDate(monday)),
-        sunday: new Date(d.setDate(sunday))
+        monday: new Date(date.setDate(monday)),
+        sunday: new Date(date.setDate(sunday))
     }
+}
+
+export function slashedDate(date){
+    const day = date.getDate();
+    const month = date.getMonth()+1;
+    return `${day}/${month}`
+}
+
+export function slashedWeek({monday, sunday}){
+    const first = slashedDate(monday);
+    const last = slashedDate(sunday);
+    return `${first} - ${last}`
+}
+
+export function weeklyLabel(d){
+    const currentWeekDuration = getWeek(d);
+    const previousWeekDuration = getWeek(new Date().setDate(d.getDate() - 7));
+    const currentWeek = slashedWeek(currentWeekDuration);
+    const previousWeek = slashedWeek(previousWeekDuration);
+    return {currentWeek, previousWeek};
 }
