777600509dd29af3e8a097eef2c15064eb8f6a17
Anton Semenkov <k3gbdia1@gmail.com>
2021-12-25 13:19:41 +0300

    fix [weekly-reports]: added filter selection button component for old reports

--- src/modules/weekly-report-history/selecting-report-characteristics.component.jsx
+++ src/modules/weekly-report-history/selecting-report-characteristics.component.jsx
@@ -1,66 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useStore } from 'effector-react';
 import { userStore } from '../store/user-store';
 import { getOldTeamReports } from '../store/old-reports-store';
+import { SelectingReportButton } from '../common/components/buttons/selecting-report-button.component';
 
 export function SelectingReportCharacteristics({ team }) {
+    const [activeButton, setButton] = useState('overall');
     const userInDB = useStore(userStore);
     const apiParameters = {
         companyId: userInDB.companyId,
         memberId: userInDB.id,
         team: team,
     };
+    const filters = ['overall', 'morale', 'stress', 'workload'];
+
     return (
         <ul className='pagination pagination-sm my-3'>
-            <li className='page-item'>
-                <button
-                    className='page-link bg-dark'
-                    onClick={() =>
-                        getOldTeamReports({
-                            ...apiParameters,
-                            filter: 'overall',
-                        })
-                    }>
-                    Overall
-                </button>
-            </li>
-            <li className='page-item'>
-                <button
-                    className='page-link bg-dark'
-                    onClick={() =>
-                        getOldTeamReports({
-                            ...apiParameters,
-                            filter: 'morale',
-                        })
-                    }>
-                    Morale
-                </button>
-            </li>
-            <li className='page-item'>
-                <button
-                    className='page-link bg-dark'
-                    onClick={() =>
-                        getOldTeamReports({
-                            ...apiParameters,
-                            filter: 'stress',
-                        })
-                    }>
-                    Stress
-                </button>
-            </li>
-            <li className='page-item'>
-                <button
-                    className='page-link bg-dark'
-                    onClick={() =>
-                        getOldTeamReports({
-                            ...apiParameters,
-                            filter: 'workload',
-                        })
-                    }>
-                    Workload
-                </button>
-            </li>
+            {filters.map((f, index) => (
+                <SelectingReportButton
+                    filter={f}
+                    selectedFilter={activeButton}
+                    apiParameters={apiParameters}
+                    setFilter={setButton}
+                    key={index}
+                />
+            ))}
         </ul>
     );
 }
