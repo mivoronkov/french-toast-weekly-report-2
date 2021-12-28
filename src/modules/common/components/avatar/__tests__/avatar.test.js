909ba4719872683c33539b3700f717e25aa2685e
Alexander Slesarenko <dragongling@gmail.com>
2021-12-28 10:36:46 +0500

    test: avatar image test added

--- src/modules/common/components/avatar/__tests__/avatar.test.js
+++ src/modules/common/components/avatar/__tests__/avatar.test.js
@@ -1,21 +1,30 @@
 import { AvatarComponent } from '../avatar.component';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+jest.mock('../../../../../utils', () => {
+    return {
+        getImageIfSrcValid: () => {
+            return Promise.resolve();
+        },
+    };
+});
 
 describe('Avatar tests', () => {
     it('Renders properly', () => {
         render(<AvatarComponent lastName='LastName' />);
     });
-    // I don't know how to test if <img> exists properly
-    // it('Shows an image if link is valid', () => {
-    //     render(
-    //         <AvatarComponent
-    //             firstName='Alexander'
-    //             lastName='Slesarenko'
-    //             avatarPath='https://avatars.githubusercontent.com/u/3900938'
-    //         />
-    //     );
-    // });
+    it('Shows an image if link is valid', async () => {
+        const dom = render(
+            <AvatarComponent
+                firstName='Alexander'
+                lastName='Slesarenko'
+                avatarPath='https://avatars.githubusercontent.com/u/3900938'
+            />
+        );
+        await waitFor(() =>
+            expect(dom.container.getElementsByTagName('img')).toHaveLength(1)
+        );
+    });
     it('Shows right letters', () => {
         render(<AvatarComponent lastName='LastName' />);
         expect(screen.getByText('LA')).toBeInTheDocument();
